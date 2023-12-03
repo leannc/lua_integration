33c784958b58672e47ad19de1490aa22c22760f8
wangkang <hust.wangkang@gmail.com>
2023-12-03 22:24:38 +0800

    [v0.0.2]使用最简单的语句，测试了一下lua的调用，成功，说明lua集成成功。

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,27 @@
 //
 #include <iostream>
 
+extern "C" {
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+}
+
+
 int main()
 {
+    lua_State *L=luaL_newstate();
+    luaL_openlibs(L);
+
+    //调用执行lua脚本
+    int r= luaL_dofile(L,"scripts/test_1.lua");
+    if (r!=LUA_OK)
+    {
+        std::string err= lua_tostring(L,-1);
+        std::cout << err << std::endl;
+        return false;
+    }
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
